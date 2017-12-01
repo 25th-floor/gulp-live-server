0fc0deb7e2e381d58bf9f21b53bc6b5ff3823253
Philipp Bisson <pb@25th-floor.com>
2017-12-01 17:27:30 +0100

    replace connect with express

--- scripts/static.js
+++ scripts/static.js
@@ -1,5 +1,6 @@
-var app = require('connect')();
-var cors = require('cors-connect').accept;
+var express = require('express')
+var cors = require('cors')
+var app = express()
 var path = require('path');
 var connect_livereload = require('connect-livereload');
 var serve_static = require('serve-static');
@@ -8,11 +9,10 @@ var args = Array.prototype.slice.call(process.argv, 2, 4);
 var root = args[0] || 'build/';
 var port = args[1] || 3000;
 
-app.use(cors.acceptNext);
-app.options('*', function(req, res){
-    return res.end();
-});
+app.use(cors());
 app.use(connect_livereload());
+
+app.options('*', cors()) // enable pre-flight request for DELETE request
 
 root.split(",").forEach(function(r){
     app.use(serve_static(path.join(process.cwd(), r)));
